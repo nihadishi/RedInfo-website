c4737e2c74ebe7db86e66e47814a3fbe03dcf2cb
Nihad <nihadishi@gmail.com>
2024-01-05 00:02:04 +0400

    init

---
+++ src/constants/footer-info.js
@@ -0,0 +1,6 @@
+export const FOOTER_TITLES = [
+    {text: "GitHub", url: "https://github.com/vitorfnery/nihadishi"},
+    {text: "LinkedIn", url: "https://www.linkedin.com/in/nihadisi/"}
+]
+
+export const FOOTER_CONTACT_BTN = {text: "Contact us"}
---
+++ src/constants/navbar-info.js
@@ -0,0 +1,7 @@
+export const NAVBAR_TITLES = [
+        {text: "Home", to: "/"}, 
+        {text: "Team", to: "/features"},
+        {text: "About", to: "/contribute"}
+]
+
+export const NAV_REQUEST_BTN = {text: "Main"}
